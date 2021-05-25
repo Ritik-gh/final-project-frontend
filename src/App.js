b65ef338a4bc81609a1a0d2edce3c16b082a1ed3
Ritik <rpsritik@gmail.com>
2021-05-25 22:34:25 +0530

    login fix, css setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import Header from "./components/helper/Header";
 import Router from "./router.js";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/css/app.css";
 // import Router from "./router.js";
 
 const App = () => {
